24b4325786da51c80d9b6c44c75d6180e2be5b74
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-09-07 16:48:58 -0700

    [DEV] Pre KeyShot Scripts _ WIP_02

--- plugins/KeyShot/Pre_KeyShot_Script.py
+++ plugins/KeyShot/Pre_KeyShot_Script.py
@@ -1,115 +1,154 @@
 import os
+from functools import partial
 
-from System import *
-from System.Diagnostics import *
-from System.IO import *
-from System.Text import *
-from Deadline.Scripting import *
-from FranticX.Processes import *
-from Deadline.Plugins import *
+#######################################################################################################################
+# GET/SET global variables
+#######################################################################################################################
 
-
-######################################################################
-## GET/SET global variables
-######################################################################
-
-def getScenePath(deadlinePlugin):
-
-    deadlinePlugin.LogInfo("    Constructing file paths ")
-
-    baseScenePath = deadlinePlugin.GetPluginInfoEntry("SceneFile")
-    baseScenePath = baseScenePath.replace("\\", "/")
-    directoryPath, sceneFileName = os.path.split(baseScenePath)
-    constScenePath = os.path.join(directoryPath, "EDITED_" + sceneFileName)
-
-    deadlinePlugin.LogInfo("    Original Scene Path : %s " % baseScenePath)
-    deadlinePlugin.LogInfo("    Edited Scene Path : %s " % constScenePath)
-
-    return baseScenePath, constScenePath
+FILE_READ_ENCODING="rb"
+FILE_WRITE_ENCODING="wb+"
 
 
-# def targetDataBlock(FILE_PATH):
-#
-#     END_BLOCK_LINE_NUM = None
-#     END_LINE_STRING = "luxappm.so"
-#
-#     # reading data file and inject lines to list
-#     dataFile = open(FILE_PATH, "rb")
-#     dataList = dataFile.readlines()
-#     dataFile.close()
-#
-#     for counter, line in enumerate(dataList):
-#         if END_LINE_STRING in line:
-#             END_BLOCK_LINE_NUM = counter
-#
-#     return dataList[:END_BLOCK_LINE_NUM]
-#
-# def constructSceneFile(FILE_PATH, OUTPUT_PATH):
-#
-#     TARGET_DATA_BLOCK = targetDataBlock(FILE_PATH)
-#
-#     outFile = open(OUTPUT_PATH, "wb+")
-#     outFile.writelines(TARGET_DATA_BLOCK)
-#     #outFile.writelines(MAIN_FILE)
-#     outFile.close()
-#
-# #constructSceneFile(FILE_PATH, OUTPUT_PATH)
-#
-#
-# # dataFile = open(FILE_PATH, "rb")
-# # i = 0
-# #
-# # for line in dataFile:
-# #     if i <= 10:
-# #         print(line)
-# #         i += 1
-#
-# from functools import partial
-#
-# with open(FILE_PATH, "rb") as f_in:
-#
-#     part_read = partial(f_in.read, 50000000*5)
-#     iterator = iter(part_read, b'')
-#
-#     #outFile = open(OUTPUT_PATH, "wb+")
-#
-#     for index, block in enumerate(iterator, start=1):
-#         #block = process_block(block)    # process block data
-#         with open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_{}.bip".format(index), "wb+") as f_out:
-#             f_out.write(block)
-#
-# outFile = open(OUTPUT_PATH, "wb+")
-#
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_1.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_2.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_3.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_4.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_5.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_6.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_7.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_8.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_9.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_10.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_11.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_12.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_13.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_14.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_15.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_16.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_17.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_18.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_19.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_20.bip", "rb").readlines())
-# outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_21.bip", "rb").readlines())
-#
-#
-# outFile.close()
+class FileParser:
 
-def __main__( *args ):
-    print "Running KeyShot Pre Script"
-    deadlinePlugin = args[0]
-    print args
-    getScenePath(deadlinePlugin)
+    def __init__(self, DeadlinePlugin):
+
+        self.DeadlinePlugin = DeadlinePlugin
+        self.baseScenePath, self.constScenePath, self.directoryPath = self.getScenePath()
+
+
+    def getScenePath(self):
+
+        self.DeadlinePlugin.LogInfo("    Constructing file paths ")
+
+        baseScenePath = self.DeadlinePlugin.GetPluginInfoEntry("SceneFile")
+        baseScenePath = baseScenePath.replace("\\", "/")
+        directoryPath, sceneFileName = os.path.split(baseScenePath)
+        directoryPath = directoryPath.replace("\\", "/")
+        constScenePath = os.path.join(directoryPath, "EDITED_" + sceneFileName)
+        constScenePath = constScenePath.replace("\\", "/")
+
+        self.DeadlinePlugin.LogInfo("    Original Scene Path : %s " % baseScenePath)
+        self.DeadlinePlugin.LogInfo("    Edited Scene Path : %s " % constScenePath)
+
+        return baseScenePath, constScenePath, directoryPath
+
+
+    # def getDataBlock(baseScenePath):
+    #
+    #     END_BLOCK_LINE_NUM = None
+    #     END_LINE_STRING = "luxappm.so"
+    #
+    #     # reading data file and inject lines to list
+    #     dataFile = open(baseScenePath, "rb")
+    #     dataList = dataFile.readlines()
+    #     dataFile.close()
+    #
+    #     for counter, line in enumerate(dataList):
+    #         if END_LINE_STRING in line:
+    #             END_BLOCK_LINE_NUM = counter
+    #
+    #     return dataList[:END_BLOCK_LINE_NUM]
+    #
+    # def createChunckDirectory(directoryPath):
+    #
+    #     DIR_STRING = "CHUNKED_FILES"
+    #     chunkDirectory = os.path.join(directoryPath, DIR_STRING)
+    #
+    #     if os.path.exists(chunkDirectory):
+    #         chunkDirectory = os.path.join(directoryPath, DIR_STRING).replace("\\", "/")
+    #         print "Chunk Directory Exists : %s" % chunkDirectory
+    #
+    #     else:
+    #         try:
+    #             os.mkdir(chunkDirectory)
+    #         except OSError:
+    #             print ("Creation of the chunk directory %s failed : " % chunkDirectory)
+    #         else:
+    #             print ("Chunk Directory successfully created : %s " % chunkDirectory)
+    #
+    #     return chunkDirectory
+    #
+    #
+    # def getChunkName(chunkIndex, chunkDirectory):
+    #
+    #     CHUNK_STRING = "Chunk_"
+    #     CHUNK_EXTENSION = ".bip"
+    #
+    #     chunkNameString = os.path.join(chunkDirectory, CHUNK_STRING, chunkIndex + CHUNK_EXTENSION).replace("\\", "/")
+    #     print "Chunk name string created : %s" % chunkNameString
+    #     return chunkNameString
+    #
+    #
+    # def constructSceneFile(baseScenePath, constScenePath, directoryPath):
+    #
+    #     FILE_READ_ENCODING = "rb"
+    #     FILE_WRITE_ENCODING = "wb+"
+    #
+    #     TARGET_DATA_BLOCK = getDataBlock(baseScenePath)
+    #     DATA_CHUNK_SIZE = 50000000*5
+    #
+    #     chunkDirectory = createChunckDirectory(directoryPath)
+    #
+    #     # constFile = open(constScenePath, "wb+")
+    #     # constFile.writelines(TARGET_DATA_BLOCK)
+    #     # constFile.writelines(baseScenePath)
+    #     # constFile.close()
+    #
+    #     with open(baseScenePath, FILE_READ_ENCODING) as inputFile:
+    #
+    #         chunkRead = partial(inputFile.read, DATA_CHUNK_SIZE)
+    #         iterator = iter(chunkRead, b'')
+    #         chunkList = []
+    #
+    #         for INDEX, dataBlock in enumerate(iterator):
+    #
+    #             # TODO : block = process_block(block)    # process block data
+    #             chunkName = getChunkName(INDEX, chunkDirectory)
+    #             with open(chunkName, FILE_WRITE_ENCODING) as outputChunk:
+    #                 outputChunk.write(dataBlock)
+    #                 chunkList.append(chunkName)
+    #                 print "New chunk written to disc : %s " % outputChunk
+    #
+    #     return assembleOutputFile(chunkList, constScenePath)
+    #
+    # def assembleOutputFile(chunkList, constScenePath):
+    #
+    #     outFile = open(constScenePath, FILE_WRITE_ENCODING)
+    #
+    #     for chunk in chunkList:
+    #         print "Assembling : %s" % chunk
+    #
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_1.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_2.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_3.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_4.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_5.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_6.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_7.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_8.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_9.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_10.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_11.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_12.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_13.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_14.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_15.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_16.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_17.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_18.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_19.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_20.bip", "rb").readlines())
+    #         # outFile.writelines(open("C:/Users/hamed.MRB/Desktop/Python_NPP_Test/chunk/chunck_21.bip", "rb").readlines())
+    #
+    #     outFile.close()
+
+
+def __main__(*args):
+
+    print "Running KeyShot Pre Job Script"
+    DeadlinePlugin = args[0]
+    FileParser(DeadlinePlugin)
 
 
 
