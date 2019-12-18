b2e58a5687ffd2a2019ba3bd88fda4ace337051f
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-17 21:00:56 -0800

    [DEV] add test pulse script

---
+++ custom/scripts/Pulse/pulse_test.py
@@ -0,0 +1,12 @@
+import clr
+import sys
+import time
+
+from System.Diagnostics import *
+from System.IO import *
+
+from Deadline.Scripting import *
+
+def __main__(*args):
+
+    print "Running Test Pulse Script"
