65a7638d05182cbf139fa94592ec2c8c4a656ea1
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-22 11:35:20 -0800

    [CLEANUP] code cleanup

--- custom/plugins/Aria/Pre_Aria_Script.py
+++ custom/plugins/Aria/Pre_Aria_Script.py
@@ -1,5 +1,19 @@
 import os
+from enum import Enum
 
+
+class SystemOptions(Enum):
+
+    directlink = "SYS_directlink"
+    filedate = "SYS_filedate"
+    filename = "SYS_filename"
+    find = "SYS_find"
+    jid = "SYS_jid"
+    link = "SYS_link"
+    susspend = "SYS_susspend"
+    uid = "SYS_uid"
+    username = "SYS_username"
+    userpath = "SYS_userpath"
 
 class PrepDownloadEnv:
 
@@ -11,6 +25,9 @@ class PrepDownloadEnv:
         self.job = self.DeadlinePlugin.GetJob()
         self.DeadlinePlugin.LogInfo("Current Job ID : %s " % str(self.job.JobId))
 
+        for option in SystemOptions:
+            self.DeadlinePlugin.LogInfo("System Options : ")
+            self.DeadlinePlugin.LogInfo("Key:%s | Value:%s" % (option, self.GetPluginInfoEntry(option)))
 
 def __main__(*args):
 
