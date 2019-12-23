309a8c1494ea3080b3135ef56fcb96626021b52c
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-22 23:39:50 -0800

    [FIX] fix deadline connect path

--- custom/plugins/Aria/Pre_Aria_Script.py
+++ custom/plugins/Aria/Pre_Aria_Script.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(r"A:\DeadlineRepository10\api\python\Deadline")
+sys.path.append(r"D:\DeadlineRepository10\api\python\Deadline")
 
 for item in sys.path:
     print item
