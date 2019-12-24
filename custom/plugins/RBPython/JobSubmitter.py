eb56a932a2b58eb73cd0832f1b298bd5ce9e4cd0
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-24 10:19:02 -0800

    [DEV] python job submitter

--- custom/plugins/RBPython/JobSubmitter.py
+++ custom/plugins/RBPython/JobSubmitter.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import requests
+from pprint import pprint
 
 import Deadline.DeadlineConnect as Connect
 conn = Connect.DeadlineCon('localhost', 1234)
@@ -11,13 +12,15 @@ def get_job_data(job_code):
 
     token_id = os.environ['SOCKET_ID']
 
-    url = 'https://get_data'
-    body = {'': ''}
-    headers = {'': ''}
+    url = 'https://api.renderboost.com/node/demand/job-data'
+    body = {'jobcode': job_code}
+    headers = {'token': token_id}
 
     request_data = requests.post(url, data=json.dumps(body), headers=headers)
+    pprint(request_data)
 
     return request_data
+
 
 def create_aria_job(system_options):
 
@@ -25,7 +28,11 @@ def create_aria_job(system_options):
                "Frames": "1",
                "Priority": 100,
                "Plugin": "Aria",
-               "BatchName": "Test_Batch"}
+               "BatchName": "Test_Batch",
+               "Whitelist": "S11",
+               "MachineLimit": 1,
+               "OutputDirectory0": "A:/AriaTest",
+               "PreJobScript": "A:/DeadlineRepository10 / custom / plugins / Aria / Pre_Aria_Script.py"}
 
     PluginInfo = {'OutputDirectory': '',
                   'DownloadLink': '',
@@ -38,21 +45,15 @@ def create_aria_job(system_options):
                   'ServerTimeStamp': True,
                   'Timeout': 60}
 
-    """
-    Name = Laptop Animation_NORMAL
-    UserName = renderboost
-    Whitelist = S11
-    MachineLimit = 1
-    MachineName = S11
-    OutputDirectory0 = A:/AriaTest
-    PreJobScript = A:/DeadlineRepository10/custom/plugins/Aria/Pre_Aria_Script.py
-    """
-
     try:
         new_job = conn.Jobs.SubmitJob(JobInfo, PluginInfo)
         print("Job created with id {}".format(new_job['_id']))
     except Exception as _err:
         print("Submission failed: %s" % _err)
+
+
+def create_render_job(job_info, plugin_info):
+    pass
 
 
 def submit_jobs(*args):
@@ -64,8 +65,13 @@ def submit_jobs(*args):
     # jobs_data = get_job_data(args[1])
 
     # create aria job
-    system_options = list()
+    system_options = dict()
     create_aria_job(system_options)
+
+    # create render job
+    job_options = dict()
+    plugin_options = dict()
+    create_render_job(job_options, plugin_options)
 
 
 if __name__ == "__main__":
