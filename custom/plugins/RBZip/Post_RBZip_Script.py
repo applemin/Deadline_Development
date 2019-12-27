6d47a72ed3b0dde4a57b1babfce1ba23355f7a69
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-26 23:47:10 -0800

    [UPDATE] adding zip post file script

---
+++ custom/plugins/RBZip/Post_RBZip_Script.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from Deadline.Scripting import ClientUtils, FrameUtils, PathUtils, RepositoryUtils
+
+def update_scene_file(DeadlinePlugin):
+    pass
+
+
+
+def __main__(*args):
+
+    print " Running RBZip Post Job Script"
+    DeadlinePlugin = args[0]
+    update_scene_file(DeadlinePlugin)
