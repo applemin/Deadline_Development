aa80f505f1dbd064c039e43d47dc45d1ab778dd9
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-27 11:33:15 -0800

    [DEV] cleanup zip post job

--- custom/plugins/RBZip/Post_RBZip_Script.py
+++ custom/plugins/RBZip/Post_RBZip_Script.py
@@ -7,11 +7,7 @@ from System.Diagnostics import *
 from Deadline.Scripting import *
 from Deadline.Jobs import *
 from System.IO import *
-
-
-
 from Deadline.Plugins import *
-from Deadline.Scripting import *
 
 
 def get_scene_file(DeadlinePlugin):
@@ -34,6 +30,7 @@ def get_scene_file(DeadlinePlugin):
     else:
         raise Exception("Scene file path not found. %s" % scene_file_path)
 
+
 def update_scene_file(DeadlinePlugin):
 
     current_job = DeadlinePlugin.GetJob()
@@ -47,62 +44,6 @@ def update_scene_file(DeadlinePlugin):
 
     target_job.SetJobPluginInfoKeyValue("SceneFile", get_scene_file(DeadlinePlugin))
     RepositoryUtils.SaveJob(target_job)
-
-
-def __main__(*args):
-
-    print("Script Started...")
-
-    MIN_COMPLETED_TASKS = 0  # Min - Number of Completed Tasks BEFORE the job is queried. Change as applicable
-
-    for job in RepositoryUtils.GetJobs(True):
-        # Filter out non-"Active" jobs
-        if job.JobStatus != "Active":
-            continue
-
-        print("JobStatus: %s" % job.JobStatus)
-
-        jobId = job.JobId
-        print("JobId: %s" % jobId)
-
-        jobName = job.JobName
-        print("JobName: %s" % jobName)
-
-        JobTaskCount = job.JobTaskCount
-        print("JobTaskCount: %s" % JobTaskCount)
-
-        jobCompletedChunks = job.CompletedChunks
-        print("JobCompletedChunks: %s" % jobCompletedChunks)
-
-        job = RepositoryUtils.GetJob(jobId, True)
-        tasks = RepositoryUtils.GetJobTasks(job, True)
-        stats = JobUtils.CalculateJobStatistics(job, tasks)
-
-        jobAverageFrameRenderTime = stats.AverageFrameRenderTime
-        jobPeakRamUsage = stats.PeakRamUsage / 1024 / 1024
-
-        print("JobAverageFrameRenderTime: %s" % jobAverageFrameRenderTime)
-        print("JobPeakRamUsage: %s" % jobPeakRamUsage)
-
-        if jobCompletedChunks >= MIN_COMPLETED_TASKS:
-            if not jobAverageFrameRenderTime.Equals(TimeSpan.Zero):
-                if jobPeakRamUsage != 0:
-
-                    timeSpan = jobAverageFrameRenderTime
-                    timeSpan = "%02dd:%02dh:%02dm:%02ds" % (timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds)
-
-                    job.ExtraInfo2 = str(timeSpan)
-                    job.ExtraInfo3 = str(jobPeakRamUsage) + "Mb"
-
-                    RepositoryUtils.SaveJob(job)
-                else:
-                    print("Job Peak Ram Usage is 0Mb at this time, skipping check until next scan...")
-            else:
-                print("Job Average Frame Render Time is 00:00:00 at this time, skipping check until next scan...")
-        else:
-            print("Min Number of Completed Tasks: %s not yet reached, skipping check until next scan..." % MIN_COMPLETED_TASKS)
-
-    print("...Script Completed")
 
 
 def __main__(*args):
