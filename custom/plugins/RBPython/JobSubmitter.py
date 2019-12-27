78b219df2b1f0128a10300604b9c793baba4b8ac
Hamed Hematyar <hamedhematyar91@gmail.com>
2019-12-26 23:11:49 -0800

    [UPDATE] re write submitter in a module

--- custom/plugins/RBPython/JobSubmitter.py
+++ custom/plugins/RBPython/JobSubmitter.py
@@ -186,8 +186,8 @@ class Submitter:
         for (dir_path, dir_names, file_names) in os.walk(base_job_dir):
             print "Looking for scene file `%s` in `%s` " % (self.find, base_job_dir)
             scene_file_path = [os.path.join(dir_path, _file) for _file in file_names if _file == self.find]
-            print "Scene file path found : %s " % scene_file_path
-        if scene_file_path:
+            print "Scene file path result : %s " % scene_file_path
+        if os.path.isfile(scene_file_path):
             return scene_file_path[0]
         else:
             raise Exception("Scene file path not found. %s" % scene_file_path)
