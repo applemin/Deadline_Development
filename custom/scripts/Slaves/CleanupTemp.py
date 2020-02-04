17764281665f1c332cbf8903578685b2593d859e
Hamed Hematyar <hamedhematyar91@gmail.com>
2020-02-04 07:08:36 -0800

    [DEV] temp cleaup command for slaves

---
+++ custom/scripts/Slaves/CleanupTemp.py
@@ -0,0 +1,14 @@
+from System.IO import *
+from Deadline.Scripting import *
+
+
+def __main__():
+    selectedSlaveInfoSettings = MonitorUtils.GetSelectedSlaveInfoSettings()
+
+    # Get the list of selected machine names from the slave info settings.
+    machineNames = SlaveUtils.GetMachineNameOrIPAddresses(selectedSlaveInfoSettings)
+
+    temp_folder = r"C:\Users\mrb\Desktop\delete"
+    for machineName in machineNames:
+        print "Running on Machine : %s" % machineName
+        SlaveUtils.SendRemoteCommand(machineName, "Execute cmd rmdir /Q /S \"" + temp_folder + "\"")
